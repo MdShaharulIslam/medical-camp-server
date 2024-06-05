0c0baa37387f53156b84fd26b79b36f7f81120cf
Md shaharul Islam <shaharulislam025@gmail.com>
2024-06-05 12:45:08 +0600

    organixer added

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 const stripe = require('stripe')(process.env.STRIPE_SERVER_KEY);
 
-
 // Middleware
 app.use(express.json());
 app.use(cors());
@@ -25,8 +24,7 @@ async function run() {
   try {
     // await client.connect();
 
-    
-    const database = client.db("MediCamp")
+    const database = client.db("MediCamp");
     const usersCollection = database.collection("users");
     const campsCollection = database.collection("camps");
     const participantsCollection = database.collection("participants");
@@ -35,111 +33,119 @@ async function run() {
     const paymentCollection = database.collection("payment");
     const feedbackCollection = database.collection("feedback");
 
-
-    app.post('/jwt', async(req, res)=>{
+    app.post('/jwt', async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: 3600000})
-      res.send({token});
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: 3600000 });
+      res.send({ token });
     });
 
     const verifyToken = (req, res, next) => {
-      if(!req.headers.authorization){
-        return res.status(401).send({message: "unauthorized access"});
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
       }
       const token = req.headers.authorization.split(' ')[1];
       jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
-        if(err){
-          return res.status(401).send({message: "unauthorized access"})
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
         }
         req.decoded = decoded;
         next();
       });
-    }
+    };
 
-    app.post('/users', async(req, res)=>{
+    app.post('/users', async (req, res) => {
       const user = req.body;
+      
+      if (user.role === "organizer") {
+        const existingOrganizer = await usersCollection.findOne({ role: "organizer" });
+        if (existingOrganizer) {
+          return res.status(400).send({ message: "An organizer already exists." });
+        }
+      }
+
       const result = await usersCollection.insertOne(user);
       res.send(result);
-    })
+    });
 
-    app.get('/users', async(req, res) => {
+    app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray();
-      res.send(result)
-    });
-    app.get('/users/:email', async(req, res) => {
-      const email = req.params.email;
-      const query = {email: email};
-      const result = await usersCollection.findOne(query);
-      res.send(result)
+      res.send(result);
     });
 
-    app.patch('/users/:email', verifyToken, async(req, res) => {
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.patch('/users/:email', verifyToken, async (req, res) => {
       const data = req.body;
       const email = req.params.email;
-      const filter = {email: email};
+      const filter = { email: email };
       const updateDoc = {
         $set: {
           address: data.address,
           contact_email: data.contact_email,
           organization_name: data.organization_name,
-          phone: data.phone,         
+          phone: data.phone,
         }
-      }
+      };
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
 
-    app.post('/camps', async(req, res) => {
+    app.post('/camps', async (req, res) => {
       const user = req.body;
       const result = await campsCollection.insertOne(user);
       res.send(result);
     });
 
-    app.get('/camps', async(req, res) => {
+    app.get('/camps', async (req, res) => {
       const result = await campsCollection.find().toArray();
-      res.send(result)
+      res.send(result);
     });
 
-    app.get('/camps/:id', async(req, res) => {
+    app.get('/camps/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await campsCollection.findOne(query);
-      res.send(result)
+      res.send(result);
     });
 
-    app.get('/camps/organizer/:email', verifyToken, async(req, res) => {
+    app.get('/camps/organizer/:email', verifyToken, async (req, res) => {
       const email = req.params.email;
-      const query = {email : email};
+      const query = { email: email };
       const result = await campsCollection.find(query).toArray();
-      res.send(result)
-    });   
+      res.send(result);
+    });
 
-    app.patch('/camps/:id', verifyToken, async(req, res) =>{
+    app.patch('/camps/:id', verifyToken, async (req, res) => {
       const data = req.body;
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
           camp_name: data.camp_name,
           date: data.date,
           camp_fees: data.camp_fees,
           location: data.location,
-          service: data.service,        
-          healthcare: data.healthcare,        
-          audience: data.audience,        
-          description: data.description,        
+          service: data.service,
+          healthcare: data.healthcare,
+          audience: data.audience,
+          description: data.description,
         }
-      }
+      };
       const result = await campsCollection.updateOne(filter, updateDoc);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.delete('/camps/:id', verifyToken, async(req, res) =>{
+    app.delete('/camps/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await campsCollection.deleteOne(query );
-      res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      const result = await campsCollection.deleteOne(query);
+      res.send(result);
+    });
 
     app.post('/participant', verifyToken, async (req, res) => {
       const participant = req.body;
@@ -148,64 +154,63 @@ async function run() {
       const query = { _id: new ObjectId(participant.camp._id) };
 
       const updateDoc = {
-        $inc: { participants: 1},
+        $inc: { participants: 1 },
       };
 
       const updateCamps = await campsCollection.updateOne(query, updateDoc);
-      res.send({joinResult, updateCamps});
+      res.send({ joinResult, updateCamps });
     });
 
-    app.get('/participant', async(req, res) => {
+    app.get('/participant', async (req, res) => {
       const result = await participantsCollection.find().toArray();
-      res.send(result)
+      res.send(result);
     });
 
-    app.get('/participant/:email', verifyToken, async(req, res) => {
+    app.get('/participant/:email', verifyToken, async (req, res) => {
       const email = req.params.email;
-      const query = {email : email};
+      const query = { email: email };
       const result = await participantsCollection.find(query).toArray();
-      res.send(result)
+      res.send(result);
     });
 
-    app.delete('/participant/:id', verifyToken, async(req, res) => {
+    app.delete('/participant/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await participantsCollection.deleteOne(query);
-      res.send(result)
-    })
-
+      res.send(result);
+    });
 
     app.post('/healthcare', async (req, res) => {
       const feedback = req.body;
       const result = await healthcareCollection.insertOne(feedback);
-      res.send(result)
+      res.send(result);
     });
 
-    app.get('/healthcare', async(req, res) => {
+    app.get('/healthcare', async (req, res) => {
       const result = await healthcareCollection.find().toArray();
-      res.send(result)
+      res.send(result);
     });
 
     app.post('/organizer', async (req, res) => {
       const feedback = req.body;
       const result = await organizerCollection.insertOne(feedback);
-      res.send(result)
+      res.send(result);
     });
 
-    app.get('/organizer', async(req, res) => {
+    app.get('/organizer', async (req, res) => {
       const result = await organizerCollection.find().toArray();
-      res.send(result)
+      res.send(result);
     });
 
     app.post('/feedback', verifyToken, async (req, res) => {
       const feedback = req.body;
       const result = await feedbackCollection.insertOne(feedback);
-      res.send(result)
+      res.send(result);
     });
 
-    app.get('/feedback', async(req, res) => {
+    app.get('/feedback', async (req, res) => {
       const result = await feedbackCollection.find().toArray();
-      res.send(result)
+      res.send(result);
     });
 
     // Payment
@@ -220,11 +225,11 @@ async function run() {
 
       res.send({
         clientSecret: paymentIntent.client_secret
-      })
+      });
     });
 
     app.get('/payments/:email', verifyToken, async (req, res) => {
-      const query = { email: req.params.email }
+      const query = { email: req.params.email };
       if (req.params.email !== req.decoded.email) {
         return res.status(403).send({ message: 'forbidden access' });
       }
@@ -243,14 +248,12 @@ async function run() {
       };
       const updateDoc = {
         $set: {
-          payment_status: "paid",          
+          payment_status: "paid",
         }
-      }
+      };
       const updateResult = await participantsCollection.updateOne(query, updateDoc);
-      res.send({paymentResult, updateResult});
+      res.send({ paymentResult, updateResult });
     });
-
-
 
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -259,8 +262,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
 
 app.get('/', (req, res) => {
   res.send("Welcome to MediCamp Server");
